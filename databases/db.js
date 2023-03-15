c0936b440a70a7c69fd1f066290ebd476a857785
moa <moa@moa.com>
2023-03-15 04:00:09 +0100

    updates

--- databases/db.js
+++ databases/db.js
@@ -3,7 +3,10 @@ const DBLINK = process.env.MONGO_URL;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(DBLINK);
+    await mongoose.connect(DBLINK, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("MongoDB connected");
   } catch (error) {
     console.log(error.message);
